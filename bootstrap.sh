31ea1fd948c847cd0131a86fd3c2c4e74dff7a56
zbarr <zbarr@eagleseven.com>
2020-03-19 15:37:26 -0500

    changed bootstrap and config has changed for ssh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,34 @@ echo "Linked .tmux.conf"
 ln -sf ${PWD}/config ~/.ssh/config
 echo "Linked .ssh/config"
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "Cloned tpm... run prefix + I in tmux environment to complete plugin installation"
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+echo "Cloned tpm"
 
-# git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
-# echo "Cloned tmux-resurrect"
+git clone git@github.com:tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+echo "Cloned tmux-resurrect"
 
-# git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
-# echo "Cloned tmux-continuum"
+git clone git@github.com:tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+echo "Cloned tmux-continuum"
+
+git clone git@github.com:tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
+echo "Cloned tmux-yank"
+
+wget https://github.com/andy-5/wslgit/releases/download/v0.9.0/wslgit.exe -P /mnt/c/Users/zbarr
+echo "Retrieved wslgit.exe into win home dir"
+
+sudo umount /mnt/c
+sudo mount -t drvfs C: /mnt/c -o metadata
+echo "Remounted filesystem for pycharm setup"
+
+ln -sf /mnt/c/Users/zbarr ~/win
+echo "Created symlink to windows home"
+
+git clone ssh://git@bitbucket:7999/sre/app-configs.git /mnt/c/Users/zbarr/PycharmProjects/app-configs
+git clone ssh://git@bitbucket:7999/sre/apps-formula.git /mnt/c/Users/zbarr/PycharmProjects/apps-formula
+git clone ssh://git@bitbucket:7999/sre/system-formula.git /mnt/c/Users/zbarr/PycharmProjects/system-formula
+git clone ssh://git@bitbucket:7999/sre/services-formula.git /mnt/c/Users/zbarr/PycharmProjects/services-formula
+git clone ssh://git@bitbucket:7999/sre/salt-pillars.git /mnt/c/Users/zbarr/PycharmProjects/salt-pillars
+git clone ssh://git@bitbucket:7999/sre/salt-app.git /mnt/c/Users/zbarr/PycharmProjects/salt-app
+
+echo "Bootstrap complete... be sure to run tpm install (prefix + I) and add wslgit.exe to pycharm configuration"
