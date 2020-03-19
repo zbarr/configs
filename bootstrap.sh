9d17f995b8a13ebb997f6e1aeb271a2d210b6a9e
zbarr <zbarr@eagleseven.com>
2020-03-19 12:58:05 -0500

    adding new bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 DIRNAME="$(dirname "$0")"
-echo ${DIRNAME}
-
 PWD="$(pwd)"
-echo ${PWD}
 
 ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
+echo "Linked .tmux.conf"
 
 ln -sf ${PWD}/config ~/.ssh/config
+echo "Linked .ssh/config"
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Cloned tpm... run prefix + I in tmux environment to complete plugin installation"
+
+# git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+# echo "Cloned tmux-resurrect"
+
+# git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+# echo "Cloned tmux-continuum"
