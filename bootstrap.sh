4d52f3a960615f0564a8ab9ed187d2a4f481e4c7
Zach Barr <zbarr@ch0cwwdr003.eagleseven.com>
2022-08-23 14:44:20 -0500

    more updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,21 +12,21 @@ echo "Linked .ssh/config"
 ln -sf ${PWD}/.bash_aliases ~/.bash_aliases
 echo "Linked .bash_aliases"
 
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 echo "Cloned tpm"
 
-git clone git@github.com:tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
-echo "Cloned tmux-resurrect"
+# git clone git@github.com:tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+# echo "Cloned tmux-resurrect"
 
-git clone git@github.com:tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
-echo "Cloned tmux-continuum"
+# git clone git@github.com:tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+# echo "Cloned tmux-continuum"
 
-git clone git@github.com:tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
-echo "Cloned tmux-yank"
+# git clone git@github.com:tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
+# echo "Cloned tmux-yank"
 
-wget https://github.com/andy-5/wslgit/releases/download/v0.9.0/wslgit.exe -P /mnt/c/Users/zbarr --no-check-certificate
-echo "Retrieved wslgit.exe into win home dir"
+# wget https://github.com/andy-5/wslgit/releases/download/v0.9.0/wslgit.exe -P /mnt/c/Users/zbarr --no-check-certificate
+# echo "Retrieved wslgit.exe into win home dir"
 
 sudo umount /mnt/c
 sudo mount -t drvfs C: /mnt/c -o metadata
@@ -35,4 +35,10 @@ echo "Remounted filesystem for pycharm setup"
 ln -sf /mnt/c/Users/zbarr ~/win
 echo "Created symlink to windows home"
 
-echo "Bootstrap complete... be sure to run tpm install (prefix + I) and add wslgit.exe to pycharm configuration"
+sudo cp resolv.conf /etc/resolv.conf
+echo "Copied resolv.conf"
+
+sudo cp wsl.conf /etc/wsl.conf
+echo "Copied wsl.conf"
+
+echo "Bootstrap complete... be sure to run tpm install (prefix + I) and change the terminal setting to allow ctrl-shift-v for paste"
